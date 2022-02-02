a72248eba68767d4459f989e5cee97995183365e
kfortaleza <kfortaleza@uolinc.com>
2022-02-02 16:54:28 -0300

    Ajustes

--- src/br/com/fiap/TabuadaApp.java
+++ src/br/com/fiap/TabuadaApp.java
@@ -1,10 +1,34 @@
 package br.com.fiap;
 
+import java.util.Scanner;
+
+import br.com.fiap.tabuada.Tabuada;
+
 public class TabuadaApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		
+		Scanner leitor = new Scanner(System.in);
+		int multiplicando;
+		int multiplicador;
+		
+		System.out.println("---------------------------");
+		System.out.println("       T A B U A D A");		
+		System.out.println("---------------------------");		
+		System.out.println("");
+		
+		System.out.println("Qual o multiplicando? ");
+		multiplicando = leitor.nextInt();
+		
+		System.out.println("Qual o multiplicador? ");
+		multiplicador = leitor.nextInt();
+		
+		Tabuada tabuada = new Tabuada();
+		
+		tabuada.executarTabuada(multiplicando, multiplicador);
 
+		
 	}
 
 }
--- src/br/com/fiap/tabuada/Tabuada.java
+++ src/br/com/fiap/tabuada/Tabuada.java
@@ -1,5 +1,22 @@
 package br.com.fiap.tabuada;
 
 public class Tabuada {
+	
+	public void executarTabuada(int multiplicando, int multiplicador) {
+		
+		int cont = 0;
+		int produto = 0;
+		
+		while (cont < multiplicador) {
+			
+			produto = multiplicando * cont;
+			
+			System.out.println(multiplicando + " x " + cont + " = " + produto);
+			
+			cont = cont + 1;
+					
+		}
+		
+	}
 
 }
