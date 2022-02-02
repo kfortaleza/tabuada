2954ea4d913890e65b01d54528fe46ac3db71842
kfortaleza <kfortaleza@uolinc.com>
2022-02-02 16:08:11 -0300

    Ajustes

--- src/br/com/fiap/TabuadaApp.java
+++ src/br/com/fiap/TabuadaApp.java
@@ -1,6 +1,6 @@
-package tabuada;
+package br.com.fiap;
 
-public class Tabuada {
+public class TabuadaApp {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
---
+++ src/br/com/fiap/tabuada/Tabuada.java
@@ -0,0 +1,5 @@
+package br.com.fiap.tabuada;
+
+public class Tabuada {
+
+}
